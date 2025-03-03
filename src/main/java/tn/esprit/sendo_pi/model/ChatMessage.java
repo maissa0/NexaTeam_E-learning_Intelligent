960eae886ba73a94b72e465db07a53f119faf948
Mohamed Ali Belkouri <medali.b@devnet.me>
2025-03-03 14:08:04 +0100

    adding chat message model

--- src/main/java/tn/esprit/sendo_pi/model/ChatMessage.java
+++ src/main/java/tn/esprit/sendo_pi/model/ChatMessage.java
@@ -1,13 +1,36 @@
 package tn.esprit.sendo_pi.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+
+@Entity
 public class ChatMessage {
     public enum MessageType {
         CHAT, JOIN, LEAVE
     }
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private MessageType type;
     private String content;
     private String sender;
+    private LocalDateTime timestamp;
+
+    public ChatMessage(String sender, String content, LocalDateTime timestamp,MessageType type) {
+        this.sender = sender;
+        this.content = content;
+        this.timestamp = timestamp;
+    }
+    // **Default (No-Arg) Constructor**
+    public ChatMessage() {
+    }
 
     // Getters and Setters
     public MessageType getType() { return type; }
@@ -18,4 +41,7 @@ public class ChatMessage {
 
     public String getSender() { return sender; }
     public void setSender(String sender) { this.sender = sender; }
+
+    public LocalDateTime getTimestamp() { return timestamp; }
+    public void setTimestamp(LocalDateTime sender) { this.timestamp = timestamp; }
 }
